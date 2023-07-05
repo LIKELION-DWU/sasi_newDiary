03e0e0d023dffe48a1866edb66223106f69c6bb2
chaechaeyeon <00dreamadream@kakao.com>
2023-07-05 21:55:10 +0900

    b-chae posts crud, comment관련 posts로 이동, 댓글 수정전

--- sasi_newDiary_back/posts/forms.py
+++ sasi_newDiary_back/posts/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post,Comment
 
 class PostModelForm(forms.ModelForm):
 	class Meta:
 		model=Post
-		fields= ['body','photo']
+		fields= ['photo', 'content',]
+
+class CommentModelForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
--- sasi_newDiary_back/posts/urls.py
+++ sasi_newDiary_back/posts/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from posts import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 app_name="posts"
 
 urlpatterns = [
-    path('', views.post_main, name='posts'),
-    # post_create 로 수정함
-    path('create',views.post_create, name='post_create'),
+    path('', views.post_main, name='main'),
+    # 게시글
+    path('post_create',views.post_create, name='post_create'),
     path('post_list/', views.post_list, name='post_list'),
-    path('post_detail/<int:post_id>', views.post_detail, name='post_detail'),
+    path('post_detail/<int:id>', views.post_detail, name='post_detail'),
+    path('post_update/<int:id>/', views.post_update, name="post_update"),
+    path('post_delete/<int:id>/', views.post_delete, name='post_delete'),
     # 댓글 
-    path('create_comment/<int:post_id>', views.create_comment, name='create_comment'),
+    path('create_comment/<int:id>', views.create_comment, name='create_comment'),
 
 ]
 
